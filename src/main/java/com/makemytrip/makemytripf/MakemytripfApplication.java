df25f12dc0f2e222002b17589dce24905ed6c5fa
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-06-27 16:49:20 +0530

    First Commit

---
+++ src/main/java/com/makemytrip/makemytripf/MakemytripfApplication.java
@@ -0,0 +1,13 @@
+package com.makemytrip.makemytripf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemytripfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytripfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makemytrip/makemytripf/MakemytripfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makemytrip.makemytripf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemytripfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
