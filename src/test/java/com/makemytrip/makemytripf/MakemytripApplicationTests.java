c46e88416955e0ee873608ad5a1e9ab081aa9f45
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-07-01 15:44:57 +0530

    Initial Commit

--- src/test/java/com/makemytrip/makemytripf/MakemytripApplicationTests.java
+++ src/test/java/com/makemytrip/makemytripf/MakemytripApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MakemytripfApplicationTests {
+class MakemytripApplicationTests {
 
 	@Test
 	void contextLoads() {
