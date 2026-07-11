b3d818b058e20738ff9e3a9facd851337fec0301
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-07-11 20:36:12 +0530

    Established API connection

--- makemytour/src/pages/_app.tsx
+++ makemytour/src/pages/_app.tsx
@@ -6,29 +6,31 @@ import Navbar from "@/components/Navbar";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "../store";
+import { Component } from "lucide-react";
+import Head from "next/head";
 
-function LoadUser() {
-  const dispatch = useDispatch();
-
+const Myapp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
-
     if (storedUser) {
-      dispatch(setUser(JSON.parse(storedUser)));
+      store.dispatch(setUser(JSON.parse(storedUser)));
     }
-  }, [dispatch]);
+  }, []);
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
+};
 
-  return null;
-}
-
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
   return (
     <Provider store={store}>
-      <LoadUser />
-      <div className="min-h-screen">
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
+      <Head>
+        <title>MakeMyTour</title>
+      </Head>
+      <Myapp {...props}></Myapp>
     </Provider>
   );
 }
