c8047cf0a15600fc8c0b82e54d18c9266dda218f
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-07-14 17:22:17 +0530

    Updated Routing

--- src/main/java/com/makemytrip/makemytrip/controllers/UserController.java
+++ src/main/java/com/makemytrip/makemytrip/controllers/UserController.java
@@ -1,10 +1,10 @@
 package com.makemytrip.makemytrip.controllers;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.makemytrip.makemytrip.models.Users;
-import com.makemytrip.makemytrip.services.UserServices;;
+import com.makemytrip.makemytrip.services.UserServices;
 
 @RestController
 @RequestMapping("/user")
@@ -17,9 +17,20 @@ public class UserController {
     public Users login(@RequestParam String email,@RequestParam String password){
         return userServices.login(email,password);
     }
-
     @PostMapping("/signup")
     public ResponseEntity<Users> signup(@RequestBody Users user){
         return ResponseEntity.ok(userServices.signup(user));
+    }
+    @GetMapping("/email")
+    public ResponseEntity<Users> getuserbyemail(@RequestParam String email){
+        Users user = userServices.getUserByEmail(email);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping("/edit")
+    public Users editprofile(@RequestParam String id ,@RequestBody Users updatedUser){
+        return userServices.editprofile(id,updatedUser);
     }
 }
